82cfea1e7363efe310a3cd377d0c27b362762e3e
David Aycan <david.aycan@gmail.com>
2014-01-12 20:10:55 -0800

    fixed paths for devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Pinvoyage::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
-  config.action_mailer.default_url_options = { :host => 'www.pinvoyage.com' }
+  config.action_mailer.default_url_options = { :host => 'http://pinvoyage.herokuapp.com/' }
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
