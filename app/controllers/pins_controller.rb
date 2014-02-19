a92fc2470ca4a88312458da3a9c29fd5c88ba761
David Aycan <david.aycan@gmail.com>
2014-02-18 19:49:12 -0800

    add browse search

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -26,10 +26,10 @@ class PinsController < ApplicationController
       without(:user_id, current_user.id)
       order_by(:created_at, :desc)  
     end
-    # @pins = @search.results
+    @pins = @search.results
     # @pins = Kaminari.paginate_array(@search.results).page(params[:page]).per(30)
 
-    @pins = Pin.where('user_id != ?', current_user.id).order('created_at desc').page(params[:page])
+    # @pins = Pin.where('user_id != ?', current_user.id).order('created_at desc').page(params[:page])
   end
 
   def repin
