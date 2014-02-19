fecb57fb13ae7eb66bce98d02ce8f220a476293c
David Aycan <david.aycan@gmail.com>
2014-02-18 22:18:02 -0800

    put browse back to linked pagination

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -26,8 +26,8 @@ class PinsController < ApplicationController
       order_by(:created_at, :desc)  
       paginate(page: params[:page], per_page: 30)
     end
-    # @pins = @search.results
-    @pins = Kaminari.paginate_array(@search.results).page(params[:page]).per(30)
+    @pins = @search.results
+    # @pins = Kaminari.paginate_array(@search.results).page(params[:page]).per(30)
 
     # @pins = Pin.where('user_id != ?', current_user.id).order('created_at desc').page(params[:page])
   end
