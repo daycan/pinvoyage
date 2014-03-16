1087646a30f07c074d9950d1a6bfcc9c13e9befb
David Aycan <david.aycan@gmail.com>
2014-03-16 14:14:03 -0700

    added back tooltip javascript

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -1,3 +1,3 @@
-// jQuery ->
-//  $("a[rel~=popover], .has-popover").popover()
-//  $("a[rel~=tooltip], .has-tooltip").tooltip()
+jQuery(document).ready(function () {
+	$('.has_tooltip').tooltip();
+});
